7da2597857cce19a3217188044841de388742208
hostile-d <host.chel@gmail.com>
2022-12-20 22:24:36 +0400

    stage 2

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,11 +1,22 @@
 package readability;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         var line = scanner.nextLine();
-        System.out.println(line.length() > 100 ? "HARD" : "EASY");
+
+        var wordsAmount = (double) Pattern.compile("\\w+\\b").matcher(line).results().count();
+        var sentencesAmount = (double) Pattern.compile("\\w+[.!?]").matcher(line).results().count();
+
+        if (!Pattern.compile("[.!?]$").matcher(line).find()) {
+            sentencesAmount += 1;
+        }
+
+        var avgWordsPerSentence = wordsAmount / sentencesAmount;
+
+        System.out.println(avgWordsPerSentence > 10d ? "HARD" : "EASY");
     }
 }
